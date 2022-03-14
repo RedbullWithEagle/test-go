487d58991b7a771e9ad79709218b3782bfdafa13
liuchangzhou <liuchangzhou@douyu.tv>
2022-03-14 18:58:24 +0800

    add

--- main.go
+++ main.go
@@ -3,5 +3,5 @@ package main
 import "github.com/test-go/other"
 
 func main() {
-	other.PrintHeart()
+	other.TestBirthDay()
 }
---
+++ other/date.go
@@ -0,0 +1,26 @@
+package other
+
+import (
+	"fmt"
+	"time"
+)
+
+/*******************************************************
+*birthDay2Time   将年月日转化为Time类型
+ ******************************************************/
+func birthDay2Time(bir int) time.Time {
+	day := bir % 100
+	month := (bir / 100) % 100
+	year := (bir / 10000) % 10000
+	fmt.Println(fmt.Sprintf("bir:%d      yeat:%d,mon:%d,day:%d", bir, year, month, day))
+	return time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.Local)
+}
+
+func TestBirthDay() {
+	bir1 := 19880334
+	bir2 := 29991231
+	bir3 := 19880101
+	fmt.Println(birthDay2Time(bir1))
+	fmt.Println(birthDay2Time(bir2))
+	fmt.Println(birthDay2Time(bir3))
+}
