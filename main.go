978af680d03579b7e721324bb54e5e874981e6b2
liuchangzhou <liuchangzhou@douyu.tv>
2022-03-31 11:22:03 +0800

    inheritance

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import base_type "github.com/test-go/base-type"
+import (
+	"errors"
+	"fmt"
+	"time"
 
+	"github.com/test-go/other"
+)
+
+func abc() (int,error){
+	return 3,errors.New("abc")
+}
+func sum (a,b int) (err error){
+	ret,err :=abc()
+	fmt.Println(err)
+	fmt.Println(ret)
+	return
+}
 func main() {
-	base_type.TestSyncMap()
+	other.TestWithCar()
+	sum(2,3)
+	go func() {
+		fmt.Println("hello goroutine")
+	}()
+
+	time.Sleep(1*time.Minute)
 }
